ab2d4e10d95f372749e72c7f593b92270c38c823
Khushi2527 <105801925+Khushi2527@users.noreply.github.com>
2022-11-15 14:21:35 +0530

    Create 02.(f)Search_in_matrix.cpp

---
+++ 02.(f)Search_in_matrix.cpp
@@ -0,0 +1,13 @@
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    int m = A.size();
+    int n = A[0].size();
+    int lo = 0, hi = (m*n)-1;
+    
+    while(lo<=hi){
+        int mid = lo + (hi-lo)/2;
+        if(A[mid/n][mid%n]==B) return true;
+        else if(A[mid/n][mid%n]<B) lo = mid+1;
+        else hi = mid-1;
+    }
+    return false;
+}
